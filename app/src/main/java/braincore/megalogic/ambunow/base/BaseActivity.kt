da85f2497abf4e3833b5ce25feae60fb72404083
Akbar Hamaminatu <akbar@ngoding.id>
2023-08-06 03:47:00 +0700

    feat: add activity base class

---
+++ app/src/main/java/braincore/megalogic/ambunow/base/BaseActivity.kt
@@ -0,0 +1,46 @@
+package braincore.megalogic.ambunow.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+import braincore.megalogic.ambunow.utils.ext.getErrorMessage
+
+abstract class BaseActivity<B : ViewBinding, VM : ViewModel>(
+    val bindingFactory: (LayoutInflater) -> B
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+    protected abstract val viewModel: VM
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = bindingFactory(layoutInflater)
+        setContentView(binding.root)
+        initView()
+        observeData()
+    }
+
+    abstract fun initView()
+
+    open fun observeData() {
+
+    }
+
+    open fun showError(isErrorEnabled: Boolean, exception: Exception) {
+        if (isErrorEnabled) {
+            Toast.makeText(this, getErrorMessage(exception), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun enableHomeAsBack() {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+}
